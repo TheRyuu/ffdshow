d6af714ab8b449cc45e6a0b7b400b0f016c02535
drevil_xxl <drevil_xxl@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2007-01-26 10:48:28 +0000

    updated x264 encoder
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@819 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/codecs/x264/ff_x264.c
+++ src/codecs/x264/ff_x264.c
@@ -8,9 +8,10 @@
 #include "x264.h"
 #include "common/common.h"
 #include "encoder/ratecontrol.h"
+#include "../../compiler.h"
 
 void __stdcall getVersion(char *ver,const char* *license)
 {
- strcpy(ver, X264_BUILD", build date "__DATE__" "__TIME__);
+ strcpy(ver, X264_BUILD", build date "__DATE__" "__TIME__" ("COMPILER COMPILER_X64")");
  *license="";
 }
