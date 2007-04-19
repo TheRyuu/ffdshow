d7bd00717e0c8826559644c49dc2f17675d647db
h_yamagata <h_yamagata@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2007-04-19 13:39:47 +0000

    Winamp plugin: Exclude Adapt-X plugin. http://forum.doom9.org/showthread.php?p=990670#post990670
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@1110 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/audioFilters/winamp2/Twinamp2.cpp
+++ src/audioFilters/winamp2/Twinamp2.cpp
@@ -74,8 +74,9 @@ Twinamp2dspDll::Twinamp2dspDll(const ffstring &flnm):refcount(1)
  char_t filename[MAX_PATH],name[MAX_PATH],ext[MAX_PATH];
  _splitpath(flnm.c_str(),NULL,NULL,name,ext);
  _makepath(filename,NULL,NULL,name,ext);
- // DSP stacker is not compatible with ffdshow currently. Maybe ffdshow's bug, but I can't help...
- if (_strnicmp(_l("dsp_stacker.dll"),filename,16)==0)
+ // DSP stacker and Adapt-X are not compatible with ffdshow currently. Maybe ffdshow's bug, but I can't help...
+ if (_strnicmp(_l("dsp_stacker.dll"),filename,16)==0
+  || _strnicmp(_l("dsp_adaptx.dll"),filename,15)==0)
   return;
  dll=new Tdll(flnm.c_str(),NULL);
  dll->loadFunction(winampDSPGetHeaderType,"winampDSPGetHeader2");
