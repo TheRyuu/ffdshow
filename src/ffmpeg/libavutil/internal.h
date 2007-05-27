25fc1e5ba12dd40593e2eaf1f8f2e8d116eb102f
clsid2 <clsid2@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2007-05-27 11:43:38 +0000

    Added define for "__attribute__((force_align_arg_pointer))" (supported by GCC 4.2 and up)
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@1211 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/ffmpeg/libavutil/internal.h
+++ src/ffmpeg/libavutil/internal.h
@@ -30,6 +30,14 @@
 #define inline
 #endif
 
+#ifndef attribute_align_arg
+#if defined(__GNUC__) && (__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ > 1)
+#    define attribute_align_arg __attribute__((force_align_arg_pointer))
+#else
+#    define attribute_align_arg
+#endif
+#endif
+
 #ifndef attribute_used
 #if defined(__GNUC__) && (__GNUC__ > 3 || __GNUC__ == 3 && __GNUC_MINOR__ > 0)
 #    define attribute_used __attribute__((used))
