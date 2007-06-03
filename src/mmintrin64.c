28af16021bc1c7237fb2429e421431bff7a8bb61
eragon4ever <eragon4ever@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2007-06-03 16:23:19 +0000

    Updated German translations   -Inno Setup   -htmlhelp   -ffdshow
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@1238 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/mmintrin64.c
+++ src/mmintrin64.c
@@ -1,15 +1,15 @@
 // In addition to the terms of the GPL, which necessarily cover the entire project, this rather trivial file is licensed as follows:
 
 //  Copyright (C) 2007 Peter Bright
-//  
+//
 //  This software is provided 'as-is', without any express or implied
 //  warranty. In no event will the authors be held liable for any damages
 //  arising from the use of this software.
-//  
+//
 //  Permission is granted to anyone to use this software for any purpose,
 //  including commercial applications, and to alter it and redistribute it
 //  freely, subject to the following restrictions:
-//  
+//
 //  1. The origin of this software must not be misrepresented; you must not
 //     claim that you wrote the original software. If you use this software
 //     in a product, an acknowledgment in the product documentation would be
@@ -17,7 +17,7 @@
 //  2. Altered source versions must be plainly marked as such, and must not be
 //     misrepresented as being the original software.
 //  3. This notice may not be removed or altered from any source distribution.
-//  
+//
 //  Peter Bright <drpizza@quiscalusmexicanus.org>
 
 // I'd release it into the public domain if my jurisdiction had an effective and meaningful way of doing so, but it doesn't, so I won't.
