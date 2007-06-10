df0b667e5a1f41f1e9d744ba6ea4d1a528d80736
h_yamagata <h_yamagata@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2007-06-10 13:00:14 +0000

    updated openIE.js by fastplayer
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@1271 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- bin/openIE.js
+++ bin/openIE.js
@@ -1,28 +1,41 @@
-//Define variables and objects
-var objArgs = WScript.Arguments;
-var urlString = "http://ffdshow-tryout.sourceforge.net/compmgr.php?app=" + objArgs(0) + "&rev=" + objArgs(1);
-var oIE = WScript.CreateObject("InternetExplorer.Application", "IE_");
+//Check if number of passed arguments is exactly 2
+var oArgs = WScript.Arguments;
+if (WScript.Arguments.Count() != 2) {
+  var timeout = 10;
+  var title = "Error";
+  var button = 48;
+  var oWSH = WScript.CreateObject("WScript.Shell");
+  var result = oWSH.Popup("Application name and/or revision number missing!\n\nThis window will close automatically in 10 seconds...", timeout, title, button);
+  if (result == 1) { //Quit immediately after OK button was clicked
+    WScript.Quit();
+  }
+  WScript.Quit(); //Quit anyway after timeout...
+} else {
+  //Compose URL and create IE object
+  var strURL = "http://ffdshow-tryout.sourceforge.net/compmgr.php?app=" + oArgs(0) + "&rev=" + oArgs(1);
+  var oIE = WScript.CreateObject("InternetExplorer.Application", "IE_");
 
-//Customize IE window
-oIE.Left = 50; 
-oIE.Top = 100; 
-oIE.Height = 300;
-oIE.Width = 380;
-oIE.MenuBar = 0;
-oIE.ToolBar = 0;
-oIE.StatusBar = 0;
+  //Customize IE window
+  oIE.Left = 50;
+  oIE.Top = 100;
+  oIE.Height = 300;
+  oIE.Width = 380;
+  oIE.MenuBar = 0;
+  oIE.ToolBar = 0;
+  oIE.StatusBar = 0;
 
-//Finally open URL and show browser window
-oIE.Navigate(urlString);
-oIE.Visible = true;
+  //Finally open URL and show browser window
+  oIE.Navigate(strURL);
+  oIE.Visible = true;
 
-//Send scripting host to sleep
-WScript.Sleep(10000);
+  //Send scripting host to sleep
+  WScript.Sleep(10000);
 
-//Close browser window after wscript.exe wakes up
-oIE.Quit();
+  //Close browser window after wscript.exe wakes up
+  oIE.Quit();
+}
 
-//OnQuit-Event gets fired when browser window gets 
+//OnQuit-Event gets fired when browser window gets
 //closed before (click on Close-button) or after timeout
 function IE_OnQuit()
 {
