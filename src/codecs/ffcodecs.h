c36dfab3f3dc11de91e89289d5df941c89a81b27
drevil_xxl <drevil_xxl@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2007-09-05 18:44:38 +0000

    Add slice-based parallel H.264 decoding Patch by Andreas Öman % andreas A olebyn P nu %
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@1463 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/codecs/ffcodecs.h
+++ src/codecs/ffcodecs.h
@@ -268,7 +268,7 @@ static __inline bool sup_me_mv0(int x)            {return sup_lavcme(x) && x!=CO
 static __inline bool sup_cbp_rd(int x)            {return x==CODEC_ID_MPEG4;}
 static __inline bool sup_qns(int x)               {return lavc_codec(x) && sup_quantProps(x) && x!=CODEC_ID_MSMPEG4V3 && x!=CODEC_ID_MSMPEG4V2 && x!=CODEC_ID_MSMPEG4V1 && x!=CODEC_ID_WMV1 && x!=CODEC_ID_WMV2 && x!=CODEC_ID_MJPEG && x!=CODEC_ID_SNOW;}
 static __inline bool sup_threads(int x)           {return x==CODEC_ID_MPEG1VIDEO || x==CODEC_ID_MPEG2VIDEO || x==CODEC_ID_MPEG4 || x==CODEC_ID_XVID4 || x264_codec(x);}
-static __inline bool sup_threads_dec(int x)       {return x==CODEC_ID_MPEG1VIDEO || x==CODEC_ID_MPEG2VIDEO;}
+static __inline bool sup_threads_dec(int x)       {return x==CODEC_ID_MPEG1VIDEO || x==CODEC_ID_MPEG2VIDEO || x==CODEC_ID_H264;}
 static __inline bool sup_palette(int x)           {return x==CODEC_ID_MSVIDEO1 || x==CODEC_ID_8BPS || x==CODEC_ID_QTRLE || x==CODEC_ID_TSCC || x==CODEC_ID_QPEG || x==CODEC_ID_PNG;}
 
 #endif
