cc02802cc61f5fcbf3302c3108157b6bbd31fcbe
clsid2 <clsid2@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2008-01-26 17:36:32 +0000

    updated libavcodec
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@1817 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/ffmpeg/libavcodec/vc1.c
+++ src/ffmpeg/libavcodec/vc1.c
@@ -4120,6 +4120,7 @@ static int vc1_decode_end(AVCodecContext *avctx)
     av_freep(&v->acpred_plane);
     av_freep(&v->over_flags_plane);
     av_freep(&v->mb_type_base);
+    ff_intrax8_common_end(&v->x8);
     return 0;
 }
 
