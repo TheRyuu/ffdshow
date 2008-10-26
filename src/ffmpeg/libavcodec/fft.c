94a24d96baaff2d073de2e812100f91eaf335293
clsid2 <clsid2@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2008-10-26 20:17:53 +0000

    64bit fix
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@2254 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/ffmpeg/libavcodec/fft.c
+++ src/ffmpeg/libavcodec/fft.c
@@ -95,10 +95,14 @@ int ff_fft_init(FFTContext *s, int nbits, int inverse)
     has_vectors = mm_support();
     if (has_vectors & MM_SSE) {
         /* SSE for P3/P4/K8 */
+/* causes crash on 64-bit Windows */	
+#ifndef ARCH_X86_64
         s->imdct_calc = ff_imdct_calc_sse;
-        s->imdct_half = ff_imdct_half_sse;
+        /* crashes DTS decoder */
+        //s->imdct_half = ff_imdct_half_sse;
         s->fft_permute = ff_fft_permute_sse;
         s->fft_calc = ff_fft_calc_sse;
+#endif
     } else if (has_vectors & MM_3DNOWEXT) {
         /* 3DNowEx for K7 */
         s->imdct_calc = ff_imdct_calc_3dn2;
@@ -109,12 +113,6 @@ int ff_fft_init(FFTContext *s, int nbits, int inverse)
         s->imdct_calc = ff_imdct_calc_3dn;
         s->imdct_half = ff_imdct_half_3dn;
         s->fft_calc = ff_fft_calc_3dn;
-    }
-#elif defined HAVE_ALTIVEC && !defined ALTIVEC_USE_REFERENCE_C_CODE
-    has_vectors = mm_support();
-    if (has_vectors & MM_ALTIVEC) {
-        s->fft_calc = ff_fft_calc_altivec;
-        split_radix = 0;
     }
 #endif
 
