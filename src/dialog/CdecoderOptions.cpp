6680d40b6df16ee681e7478cb970a9b3d81231ea
h_yamagata <h_yamagata@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2008-12-05 11:03:30 +0000

    Decoder options page: update tooltip hint  "mpeg1/2 only" -> "H.264 and MPEG-1/2 decoder only"
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@2418 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/dialog/CdecoderOptions.cpp
+++ src/dialog/CdecoderOptions.cpp
@@ -47,7 +47,7 @@ void TmiscPage::init(void)
   enable(islavc,workarounds[i].idc_chb);
  static const int idLavc[]={IDC_LBL_IDCT,IDC_CBX_IDCT,IDC_CHB_GRAY,IDC_LBL_BUGS,IDC_LBL_ERROR_CONCEALMENT,IDC_CBX_ERROR_CONCEALMENT,IDC_LBL_ERROR_RECOGNITION,IDC_CBX_ERROR_RECOGNITION,IDC_BT_QUANTMATRIX_EXPORT,IDC_ED_NUMTHREADS,IDC_CHB_H264_SKIP_ON_DELAY,IDC_ED_H264SKIP_ON_DELAY_TIME,0};
  enable(islavc,idLavc);
- addHint(IDC_ED_NUMTHREADS,_l("mpeg1/2 decoder only"));
+ addHint(IDC_ED_NUMTHREADS,_l("H.264 and MPEG-1/2 decoder only"));
 }
 
 void TmiscPage::cfg2dlg(void)
