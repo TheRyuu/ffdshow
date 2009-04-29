9c440cc8270d824c087a5b3f5b3cc728eeb4279f
h_yamagata <h_yamagata@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2009-04-29 11:04:43 +0000

    Update boost to rev 52655
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@2920 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- bin/lib_x64/boost_thread_x64_build.cmd
+++ bin/lib_x64/boost_thread_x64_build.cmd
@@ -1,3 +1,3 @@
-REM command line used to build libboost_thread-vc90-mt-s-1_37.lib
+REM command line used to build libboost_thread-vc90-mt-s-1_39_52655.lib
 
 bjam --toolset=msvc-9.0 address-model=64 --with-thread variant=release link=static threading=multi runtime-link=static stage
