4e077a93037b20e3e4afa12374f3833850a8d8fc
h_yamagata <h_yamagata@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2009-05-28 11:48:00 +0000

    ffmpeg-mt:  bring back 2945 with fix
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@2974 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/ffmpeg-mt/libavcodec/h264.c
+++ src/ffmpeg-mt/libavcodec/h264.c
@@ -8206,7 +8206,10 @@ static int decode_frame(AVCodecContext *avctx,
 
     /* ffdshow custom code (begin) */
     pict->h264_poc_decoded = h->poc_lsb + h->poc_msb;
-    pict->h264_poc_outputed = h->outputed_poc;
+    if (h->next_output_pic)
+        pict->h264_poc_outputed = h->next_output_pic->poc;
+    else
+        pict->h264_poc_outputed = INT_MIN;
     pict->h264_frame_num_decoded = h-> frame_num;
     pict->h264_max_frame_num = 1 << h->sps.log2_max_frame_num;
     /* ffdshow custom code (end) */
