2a32155a20b176e186178478e216340296566e70
clsid2 <clsid2@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2009-09-25 13:52:23 +0000

    Updated FFmpeg
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@3088 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/ffmpeg/libavcodec/dvdata.h
+++ src/ffmpeg/libavcodec/dvdata.h
@@ -589,9 +589,8 @@ static const DVprofile dv_profiles[] = {
       /*.audio_samples_dist = */{ 1920, 1920, 1920, 1920, 1920 },
       /*.audio_shuffle = */dv_audio_shuffle625,
       "SMPTE-314M - 625/50 (PAL) 50 Mbps",
-    }
+    },
 #if 0
-    ,
     { /*.dsf = */0,
       /*.video_stype = */0x14,
       /*.frame_size = */480000,        /* SMPTE-370M - 1080i60 100 Mbps */
@@ -675,8 +674,29 @@ static const DVprofile dv_profiles[] = {
       /*.audio_samples_dist = */{ 1920, 1920, 1920, 1920, 1920 },
       /*.audio_shuffle = */dv_audio_shuffle625,
       "SMPTE-370M - 720p50 100 Mbps",
-    }
+    },
 #endif
+    { /*.dsf = */1,
+      /*.video_stype = */0x1,
+      /*.frame_size = */144000,        /* IEC 61883-5 - 625/50 (PAL) */
+      /*.difseg_size = */12,
+      /*.n_difchan = */1,
+      /*.time_base = */{ 1, 25 },
+      /*.ltc_divisor = */25,
+      /*.height = */576,
+      /*.width = */720,
+      /*.sar = */{{59, 54}, {118, 81}},
+      /*.work_chunks = */&work_chunks_dv25pal[0],
+      /*.idct_factor = */&dv_idct_factor_sd[0],
+      /*.pix_fmt = */PIX_FMT_YUV420P,
+      /*.bpm = */6,
+      /*.block_sizes = */block_sizes_dv2550,
+      /*.audio_stride = */108,
+      /*.audio_min_samples  = */{ 1896, 1742, 1264 }, /* for 48, 44.1 and 32kHz */
+      /*.audio_samples_dist = */{ 1920, 1920, 1920, 1920, 1920 },
+      /*.audio_shuffle = */dv_audio_shuffle625,
+      "IEC 61883-5 - 625/50 (PAL)",
+    }
 };
 
 enum dv_section_type {
