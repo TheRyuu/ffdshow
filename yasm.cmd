3bee0d5908ccf5ccdd563ba678e6052f02e5ef9b
tal_aloni <tal_aloni@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2010-02-12 22:07:03 +0000

    few adjustments related to rev. 3262
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@3264 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- yasm.cmd
+++ yasm.cmd
@@ -1,10 +1,9 @@
 @echo off
 REM You must have yasm.exe in a directory pointed by PATH.
-REM Source code path must not include space.
 
 if "%1" == "Win32" (
-    yasm.exe -f win32 -DPREFIX -DWIN32 -DHAVE_SSE3 -DUSE32 -Dalloc -Dexec -o %3 %4
+    yasm.exe -f win32 -DPREFIX -DWIN32 -DHAVE_SSE3 -DUSE32 -Dalloc -Dexec -I %2 -o %3 %4
 
 ) else (
-    yasm.exe -f win64 -DWIN64 -o %3 %4
+    yasm.exe -f win64 -DWIN64 -I %2 -o %3 %4
 )
