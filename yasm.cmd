fda0a69118ffc9a6f61cc33018353a49f9560e6d
tal_aloni <tal_aloni@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2010-02-12 20:26:55 +0000

    fixed yasm compilation errors when project path contains spaces (patch by babgVant)
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@3262 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- yasm.cmd
+++ yasm.cmd
@@ -3,8 +3,8 @@ REM You must have yasm.exe in a directory pointed by PATH.
 REM Source code path must not include space.
 
 if "%1" == "Win32" (
-    yasm.exe -f win32 -DPREFIX -DWIN32 -DHAVE_SSE3 -DUSE32 -Dalloc -Dexec %2 -o %3 %4
+    yasm.exe -f win32 -DPREFIX -DWIN32 -DHAVE_SSE3 -DUSE32 -Dalloc -Dexec -o %3 %4
 
 ) else (
-    yasm.exe -f win64 -DWIN64 %2 -o %3 %4
+    yasm.exe -f win64 -DWIN64 -o %3 %4
 )
