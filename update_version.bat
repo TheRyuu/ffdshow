1b92452e5a8ba9b1bff564ceca11a449ba3e8bbe
clsid2 <clsid2@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2010-03-24 14:25:14 +0000

    Further improve previous revision
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@3332 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- update_version.bat
+++ update_version.bat
@@ -1,19 +1,22 @@
 @if not exist "%programfiles%\TortoiseSVN\bin\SubWCRev.exe" goto :x64
 
 @"%programfiles%\TortoiseSVN\bin\SubWCRev.exe" .\ src\SubWCRev.conf src\svn_version.h
-@if ERRORLEVEL 6 goto :NoSubWCRev
+@if %ERRORLEVEL%==6 goto :NoSubWCRev
+@if %ERRORLEVEL%==10 goto :NoSubWCRev
 @goto :eof
 
 :x64
 @if not exist "%ProgramW6432%\TortoiseSVN\bin\SubWCRev.exe" goto :NoSubWCRev
 
 @"%ProgramW6432%\TortoiseSVN\bin\SubWCRev.exe" .\ src\SubWCRev.conf src\svn_version.h
-@if ERRORLEVEL 6 goto :NoSubWCRev
+@if %ERRORLEVEL%==6 goto :NoSubWCRev
+@if %ERRORLEVEL%==10 goto :NoSubWCRev
 @goto :eof
 
 :NoSubWCRev
+@echo NoSubWCRev
 @if exist src\svn_version.h del src\svn_version.h
-@echo #define SVN_REVISION 0 >>  src\svn_version.h
-@echo #define BUILD_YEAR 2010 >>  src\svn_version.h
-@echo #define BUILD_MONTH 1 >>  src\svn_version.h
-@echo #define BUILD_DAY 1 >>  src\svn_version.h
+@echo #define SVN_REVISION 0 >> src\svn_version.h
+@echo #define BUILD_YEAR 2010 >> src\svn_version.h
+@echo #define BUILD_MONTH 1 >> src\svn_version.h
+@echo #define BUILD_DAY 1 >> src\svn_version.h
