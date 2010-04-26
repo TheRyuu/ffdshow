06238f64525ea1722cc28e85f1727f9fd4833860
drevil_xxl <drevil_xxl@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2010-04-26 17:44:39 +0000

    updated project files.
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@3379 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- MakeAll.bat
+++ MakeAll.bat
@@ -7,6 +7,8 @@ cd ..\..\src\mplayer
 make -j4
 cd ..\..\src\codecs\x264
 make -j4
+cd ..\..\src\codecs\xvidcore
+make -j4
 REM uncomment the following 2 lines to build theora
 REM cd ..\..\..\src\codecs\theora
 REM make -j4
