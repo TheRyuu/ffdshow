8539cf411829294e487ea2c301333f1ff6c9dfdd
xhmikosr <xhmikosr@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2010-04-29 09:12:15 +0000

    updated changelog and MakeAll.bat
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@3384 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- MakeAll.bat
+++ MakeAll.bat
@@ -1,15 +1,25 @@
+@ECHO OFF
 REM Add MinGW\bin directory to PATH (Control Panel > System > Advanced > Environment Variables)
-cd src\ffmpeg
+pushd src\ffmpeg
 make -j4
-cd ..\..\src\ffmpeg-mt
+popd
+
+pushd src\ffmpeg-mt
 make -j4
-cd ..\..\src\mplayer
+popd
+
+pushd src\mplayer
 make -j4
-cd ..\..\src\codecs\x264
+popd
+
+pushd src\codecs\x264
 make -j4
-cd ..\..\src\codecs\xvidcore
+popd
+
+pushd src\codecs\xvidcore
 make -j4
+popd
+
 REM uncomment the following 2 lines to build theora
-REM cd ..\..\..\src\codecs\theora
+REM pushd src\codecs\theora
 REM make -j4
-cd ..\..\..
