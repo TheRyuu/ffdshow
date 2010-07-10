a511c3613bc0156506ad76558a29b76f40b7c69b
clsid2 <clsid2@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2010-07-10 12:48:55 +0000

    Add madVR to list of compatible renderers for dynamic format changes
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@3508 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/TffdshowDecVideo.cpp
+++ src/TffdshowDecVideo.cpp
@@ -233,6 +233,7 @@ HRESULT TffdshowDecVideo::checkAllowOutChange(IPin *pPin)
                  clsid==CLSID_DirectVobSubFilter ||
                  clsid==CLSID_DirectVobSubFilter2 ||
                  clsid==CLSID_HaaliVideoRenderer ||
+                 clsid==CLSID_MADVideoRenderer ||
                  clsid==CLSID_FFDSHOW ||
                  clsid==CLSID_FFDSHOWRAW ||
                  clsid==CLSID_FFDSHOWSUBTITLES ||
