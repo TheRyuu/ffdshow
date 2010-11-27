c7107b7bc17330987dbd7edc96c9b19500f809d0
sansnom05 <sansnom05@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2010-11-27 06:10:43 +0000

    updated boost lib to v1.45
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@3632 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- bin/lib_x64/boost_thread_x64_build.cmd
+++ bin/lib_x64/boost_thread_x64_build.cmd
@@ -1,4 +1,4 @@
-REM command line used to build libboost_thread-vc90-mt-s-1_44_64745.lib
+REM command line used to build libboost_thread-vc90-mt-s-1_45_66662.lib
 REM toolset = msvc-9.0 | msvc-10.0
 REM variant = release | debug
 
