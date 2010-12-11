bd5ac33291ba29eec8f5c938866a974d1b52c7f5
clsid2 <clsid2@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2010-12-11 22:13:46 +0000

    Removed some build scripts
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@3662 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- clean_x86_icl.bat
+++
@@ -1,8 +0,0 @@
-@SET FF_BUILD_NAME=x86_icl
-@SET BUILDTYPE=/clean
-@SET SOLUTIONFILE1=ffdshow_2008.sln
-@SET SOLUTIONFILE2=ffdshow_2008_icl10.sln
-@SET BUILDTARGET1="Release|Win32"
-@SET BUILDTARGET2="Release|Win32"
-
-@build_script.bat
