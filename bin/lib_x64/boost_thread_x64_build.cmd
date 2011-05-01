79dd971732473a2f24649b7be8fa8c56a17d8b32
XhmikosR <XhmikosR@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2011-05-01 14:08:38 +0000

    update Boost to v.1.46.1
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@3833 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- bin/lib_x64/boost_thread_x64_build.cmd
+++ bin/lib_x64/boost_thread_x64_build.cmd
@@ -1,5 +1,17 @@
-REM command line used to build libboost_thread-vc90-mt-s-1_45_66662.lib
-REM toolset = msvc-9.0 | msvc-10.0
-REM variant = release | debug
+@ECHO OFF
+rem command line used to build libboost_thread-vcTOOLSET-mt-VARIANT-1_46_1.lib
+rem You need to build bjam first running bootstrap.bat and the appropriate MSVC version
+rem toolset = msvc-9.0 | msvc-10.0
+rem variant = release | debug
 
-bjam --toolset=msvc-9.0 address-model=64 --with-thread variant=release link=static threading=multi runtime-link=static stage
+rem ECHO bjam --toolset=msvc-9.0 address-model=64 --with-thread variant=debug link=static threading=multi runtime-link=static stage
+rem bjam --toolset=msvc-9.0 address-model=64 --with-thread variant=debug link=static threading=multi runtime-link=static stage
+
+rem ECHO bjam --toolset=msvc-9.0 address-model=64 --with-thread variant=release link=static threading=multi runtime-link=static stage
+rem bjam --toolset=msvc-9.0 address-model=64 --with-thread variant=release link=static threading=multi runtime-link=static stage
+
+ECHO bjam --toolset=msvc-10.0 address-model=64 --with-thread variant=debug link=static threading=multi runtime-link=static stage
+bjam --toolset=msvc-10.0 address-model=64 --with-thread variant=debug link=static threading=multi runtime-link=static stage
+
+ECHO bjam --toolset=msvc-10.0 address-model=64 --with-thread variant=release link=static threading=multi runtime-link=static stage
+bjam --toolset=msvc-10.0 address-model=64 --with-thread variant=release link=static threading=multi runtime-link=static stage
