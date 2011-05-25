486fbb6b7053266d39b09c89f7860b3e144e4aea
clsid2 <clsid2@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2011-05-25 19:30:00 +0000

    Avoid crash of DXVA decoder with corrupt h264 stream
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@3858 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/ffmpeg/libavcodec/h264_refs.c
+++ src/ffmpeg/libavcodec/h264_refs.c
@@ -25,7 +25,6 @@
  * @author Michael Niedermayer <michaelni@gmx.at>
  */
 
-#include "libavutil/avassert.h"
 #include "internal.h"
 #include "dsputil.h"
 #include "avcodec.h"
@@ -477,8 +476,7 @@ static void print_long_term(H264Context *h) {
 
 void ff_generate_sliding_window_mmcos(H264Context *h) {
     MpegEncContext * const s = &h->s;
-    // FFmpeg patch
-    av_assert0(h->long_ref_count + h->short_ref_count <= h->sps.ref_frame_count);
+    assert(h->long_ref_count + h->short_ref_count <= h->sps.ref_frame_count);
 
     h->mmco_index= 0;
     if(h->short_ref_count && h->long_ref_count + h->short_ref_count == h->sps.ref_frame_count &&
