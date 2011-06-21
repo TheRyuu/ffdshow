5d28fab3badcc70705313c317bac2672ec3f25e1
XhmikosR <XhmikosR@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2011-06-21 13:12:20 +0000

    cleanup
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@3896 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- yasm.cmd
+++ yasm.cmd
@@ -2,8 +2,7 @@
 REM You must have yasm.exe in a directory pointed by PATH.
 
 if "%1" == "Win32" (
-    yasm.exe -f win32 -DPREFIX -DWIN32 -DHAVE_SSE3 -DUSE32 -Dalloc -Dexec -I %2 -o %3 %4
-
+  yasm.exe -f win32 -DPREFIX -DWIN32 -DHAVE_SSE3 -DUSE32 -Dalloc -Dexec -I %2 -o %3 %4
 ) else (
-    yasm.exe -f win64 -DWIN64 -I %2 -o %3 %4
+  yasm.exe -f win64 -DWIN64 -I %2 -o %3 %4
 )
