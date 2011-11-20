24d0ceb1f547948e1a61cbaf4ef49229feec06ea
clsid2 <clsid2@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2011-11-20 10:39:16 +0000

    Adjust config for gcc 4.6+
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@4052 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/ffmpeg/config.h
+++ src/ffmpeg/config.h
@@ -43,8 +43,8 @@
 #define ASMALIGN(ZEROBITS) ".p2align " #ZEROBITS "\n\t"
 
 #if ARCH_X86_64
-  #define EXTERN_PREFIX "_"
-  #define EXTERN_ASM _
+  #define EXTERN_PREFIX ""
+  #define EXTERN_ASM 
 #else
   #define EXTERN_PREFIX "_"
   #define EXTERN_ASM _
