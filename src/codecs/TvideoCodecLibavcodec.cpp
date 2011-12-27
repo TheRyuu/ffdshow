74ccdb4ef49352af640f721459825fea8d233ad2
h_yamagata <h_yamagata@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2011-12-27 13:12:38 +0000

    Update copyright
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@4204 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/codecs/TvideoCodecLibavcodec.cpp
+++ src/codecs/TvideoCodecLibavcodec.cpp
@@ -15,6 +15,10 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+/*
+ * Th264RandomAccess
+ * Copyright (c) 2008-2011 Haruhiko Yamagata
+ */
 
 #include "stdafx.h"
 
