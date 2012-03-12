f1ee2787d44c8ddfcb0e1bc74fb1bd44399f5dae
clsid2 <clsid2@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2012-03-12 15:09:04 +0000

    Updated Libav
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@4381 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/ffmpeg/libavcodec/dvdata.h
+++ src/ffmpeg/libavcodec/dvdata.h
@@ -383,26 +383,28 @@ enum dv_pack_type {
 static const DVprofile* avpriv_dv_frame_profile(const DVprofile *sys,
                                   const uint8_t* frame, unsigned buf_size)
 {
-   int i;
+    int i, dsf, stype;
 
-   int dsf = (frame[3] & 0x80) >> 7;
+    if (buf_size < 80 * 5 + 48 + 4)
+        return NULL;
 
-   int stype = frame[80*5 + 48 + 3] & 0x1f;
+    dsf = (frame[3] & 0x80) >> 7;
+    stype = frame[80 * 5 + 48 + 3] & 0x1f;
 
-   /* 576i50 25Mbps 4:1:1 is a special case */
-   if (dsf == 1 && stype == 0 && frame[4] & 0x07 /* the APT field */) {
-       return &dv_profiles[2];
-   }
+    /* 576i50 25Mbps 4:1:1 is a special case */
+    if (dsf == 1 && stype == 0 && frame[4] & 0x07 /* the APT field */) {
+        return &dv_profiles[2];
+    }
 
-   for (i=0; i<FF_ARRAY_ELEMS(dv_profiles); i++)
-       if (dsf == dv_profiles[i].dsf && stype == dv_profiles[i].video_stype)
-           return &dv_profiles[i];
+    for (i = 0; i < FF_ARRAY_ELEMS(dv_profiles); i++)
+        if (dsf == dv_profiles[i].dsf && stype == dv_profiles[i].video_stype)
+            return &dv_profiles[i];
 
-   /* check if old sys matches and assumes corrupted input */
-   if (sys && buf_size == sys->frame_size)
-       return sys;
+    /* check if old sys matches and assumes corrupted input */
+    if (sys && buf_size == sys->frame_size)
+        return sys;
 
-   return NULL;
+    return NULL;
 }
 
 static const DVprofile* avpriv_dv_codec_profile(AVCodecContext* codec)
