03afa599628b5aa74d24aab8f19293d4dcb922da
XhmikosR <XhmikosR@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2012-05-14 13:39:16 +0000

    keep one line statements
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@4449 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- src/comptr.h
+++ src/comptr.h
@@ -4,90 +4,64 @@
 template<class T> struct comptr {
 protected:
     T* p;
-    IUnknown* ptrAssign(IUnknown **pp,IUnknown *lp) {
-        if (lp!=NULL) {
-            lp->AddRef();
-        }
-        if (*pp) {
-            (*pp)->Release();
-        }
-        *pp=lp;
+    IUnknown* ptrAssign(IUnknown **pp, IUnknown *lp) {
+        if (lp != NULL) {lp->AddRef();}
+        if (*pp) {(*pp)->Release();}
+        *pp = lp;
         return lp;
     }
 public:
-    comptr(void):p(NULL) {}
+    comptr(void): p(NULL) {}
     comptr(T* lp) {
-        if ((p=lp)!=NULL) {
-            p->AddRef();
-        }
+        if ((p = lp) != NULL) {p->AddRef();}
     }
     comptr(const comptr<T> &lp) {
-        if ((p=lp.p)!=NULL) {
-            p->AddRef();
-        }
+        if ((p = lp.p) != NULL) {p->AddRef();}
     }
     ~comptr() {
-        if (p) {
-            p->Release();
-        }
+        if (p) {p->Release();}
     }
     void Release() {
-        if (p) {
-            p->Release();
-        }
-        p=NULL;
+        if (p) {p->Release();}
+        p = NULL;
     }
-    operator T*() {
-        return (T*)p;
-    }
+    operator T*() {return (T*)p;}
     T& operator*() {
-        ASSERT(p!=NULL);
+        ASSERT(p != NULL);
         return *p;
     }
     T** operator&() {
-        ASSERT(p==NULL);
+        ASSERT(p == NULL);
         return &p;
     }
     T* operator->() {
-        ASSERT(p!=NULL);
+        ASSERT(p != NULL);
         return p;
     }
-    T* operator=(T* lp) {
-        return (T*)ptrAssign((IUnknown**)&p,lp);
-    }
-    T* operator=(const comptr<T> &lp) {
-        return (T*)ptrAssign((IUnknown**)&p,lp.p);
-    }
-    bool operator!() {
-        return p==NULL;
-    }
+    T* operator=(T* lp) {return (T*)ptrAssign((IUnknown**)&p, lp);}
+    T* operator=(const comptr<T> &lp) {return (T*)ptrAssign((IUnknown**)&p, lp.p);}
+    bool operator!() {return p == NULL;}
 };
 
 template<class T> static inline const GUID& getGUID(void);
 
 #define DEFINE_TGUID(IID,I, l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8) \
- DEFINE_GUID(IID##_##I,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8);     \
- template<> inline const GUID& getGUID<I>(void) {return IID##_##I;}
+  DEFINE_GUID(IID##_##I,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8);     \
+  template<> inline const GUID& getGUID<I>(void) {return IID##_##I;}
 
-template<class T> class comptrQ :public comptr<T>
+template<class T> class comptrQ : public comptr<T>
 {
 public:
     comptrQ(void) {}
     template<class Tsrc> comptrQ(Tsrc *src) {
-        this->p=NULL;
-        if (src->QueryInterface(getGUID<T>(),(void**)&this->p)!=S_OK) {
-            this->p=NULL;
-        }
+        this->p = NULL;
+        if (src->QueryInterface(getGUID<T>(), (void**)&this->p) != S_OK) {this->p = NULL;}
     }
     template<class Tsrc> comptrQ(comptr<Tsrc> &src) {
-        this->p=NULL;
-        if (src->QueryInterface(getGUID<T>(),(void**)&this->p)!=S_OK) {
-            this->p=NULL;
-        }
+        this->p = NULL;
+        if (src->QueryInterface(getGUID<T>(), (void**)&this->p) != S_OK) {this->p = NULL;}
     }
-    T* operator=(T* lp) {
-        return (T*)ptrAssign((IUnknown**)&this->p,lp);
-    }
+    T* operator=(T* lp) {return (T*)ptrAssign((IUnknown**)&this->p, lp);}
 };
 
 template<> inline const GUID& getGUID<IMediaPosition>(void)
