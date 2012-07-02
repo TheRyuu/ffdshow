2ab34eb065fe005cd4707bcc838f418c6392becf
XhmikosR <XhmikosR@3b938f2f-1a1a-0410-8054-a526ea5ff92c>
2012-07-02 15:07:42 +0000

    update Boost to v1.50 and let the linker decide which libraries to link
    git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@4466 3b938f2f-1a1a-0410-8054-a526ea5ff92c

--- bin/lib_x64/boost_thread_x64_build_icl12.cmd
+++ bin/lib_x64/boost_thread_x64_build_icl12.cmd
@@ -1,13 +1,15 @@
 @ECHO OFF
 rem command line used to build libboost_thread-vcTOOLSET-mt-VARIANT-1_50.lib
-rem You need to build bjam first by running bootstrap.bat and the appropriate MSVC version
-SET toolset=msvc-10.0
+rem You need to build bjam first by running bootstrap.bat
+SET toolset=intel-12.1
 
 FOR %%L IN (
   "debug" "release"
 ) DO (
+  CALL "%ICPP_COMPOSER2011%..\ips-vars.cmd" ia32 vs2010
   CALL :SubBjam %%L 32 thread
   CALL :SubBjam %%L 32 date_time
+  CALL "%ICPP_COMPOSER2011%..\ips-vars.cmd" ia32_intel64 vs2010
   CALL :SubBjam %%L 64 thread
   CALL :SubBjam %%L 64 date_time
 )
